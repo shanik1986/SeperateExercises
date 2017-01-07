d05a75c2fa851fbdab0fd5fe4f33276b5a127fa6
Shani Keynan <shanik1986@gmail.com>
2017-01-07 11:05:22 +0200

    Changed medium 2

--- 101-109/medium02/testing.rb
+++ 101-109/medium02/testing.rb
@@ -1,24 +1,6 @@
-def sum_square_difference(num)
-  sum_of_squares = compute_sum_of_squares(num)
-  square_of_sums = compute_square_of_sums(num)
+sample = File.read("sample_text.txt")
+sentences = sample.split(/[\.?!]/)
+longest = sentences.max_by { |sentence| sentence.split.size }
 
-  square_of_sums - sum_of_squares
-end
-
-def compute_square_of_sums(num)
-  result = 0
-  1.upto(num)  { |i| result += i }
-  result ** 2
-end
-
-def compute_sum_of_squares(num)
-  result = 0
-  1.upto(num) { |i| result += i ** 2 }
-  result
-end
-
-p sum_square_difference(3) == 22
-   # -> (1 + 2 + 3)**2 - (1**2 + 2**2 + 3**2)
-p sum_square_difference(10) == 2640
-p sum_square_difference(1) == 0
-p sum_square_difference(100) == 25164150
+puts "The longest sentence is: \"#{longest}\""
+puts "The sentence contains #{longest.split.size} words"
